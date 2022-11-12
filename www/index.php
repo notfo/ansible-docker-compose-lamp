57d3e2524ea8093c42c5e1a5bf9ea8f14c33a509
Notfo Ltd <domain@notfo.com>
2022-11-12 14:39:24 +0300

    add  function getenv('HOSTNAME') for echo short UUID container

--- www/index.php
+++ www/index.php
@@ -1,4 +1,5 @@
 <?php
+	$docker_hostname = getenv('HOSTNAME'); 
 	$link = mysqli_connect("database", "root", $_ENV['MYSQL_ROOT_PASSWORD'], "docker");
 	if (mysqli_connect_errno()) {
 		 printf("MySQL connecttion failed: %s", mysqli_connect_error());
@@ -6,7 +7,7 @@
 	$sql = "SELECT * FROM Firstname JOIN Lastname ON Firstname.id = Lastname.id ORDER BY RAND() LIMIT 1;";
 	$result = $link->query($sql);
 	while ($row = $result->fetch_assoc()) {
-    		echo $row['firstname'].' '.$row['lastname'].' from '.$_SERVER['SERVER_NAME'];
+    		echo $row['firstname'].' '.$row['lastname'].' from '.$docker_hostname;
 	}
 	mysqli_close($link);
 ?>
